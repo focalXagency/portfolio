d24c1461faefd1083aebc06bd4015b12f6330460
FAISAL <faisalqashmr.fs@gmail.com>
2023-09-14 22:37:51 +0300

    update

--- js/lets-talk-section-script.js
+++ js/lets-talk-section-script.js
@@ -22,3 +22,29 @@ formEle.addEventListener('submit', event => {
     .then(result => console.log(result))
     .catch(error => console.log(error))
 })
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let readMore = document.querySelector('.read-more');
+    let showLess = document.querySelector('.show-less');
+    let textContainer = document.querySelector('.text-container');
+    let FFFFF = document.querySelector('.FFFFF');
+  
+    readMore.addEventListener('click', function(event) {
+      event.preventDefault();
+      textContainer.classList.add('show-more');
+      FFFFF.style.display = 'inline-block';
+      readMore.style.display = 'none';
+      showLess.style.display = 'inline-block';
+    });
+  
+    showLess.addEventListener('click', function(event) {
+      event.preventDefault();
+      textContainer.classList.remove('show-more'); 
+      FFFFF.style.display = 'none';
+      FFFFF.style.z_index = '12345';
+      showLess.style.display = 'none';
+      readMore.style.display = 'inline-block'; 
+    });
+  });
--- js/portfolio-section-script.js
+++ js/portfolio-section-script.js
@@ -3,7 +3,7 @@ new Swiper(".portfolio-swiper", {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
-	spaceBetween: 60,
+	spaceBetween: 20,
 	breakpoints: {
 		600: {
 			slidesPerView: 1,
@@ -21,107 +21,206 @@ new Swiper(".portfolio-swiper", {
 	},
 });
 
-const father = document.querySelector(".swiper-wrapper-portfolio");
-var container = document.querySelector(".port-redu");
-var btns = container.querySelectorAll(".redu");
-const subSection = document.querySelector(".redu.active");
-let lastRes;
-let currentSection  = subSection.innerText.toLowerCase().replaceAll(' ','-');//to fit with sections values from back-end
-if(currentSection === "photos-taken-by-me") { // also to fit 
-  lastRes = "photos";
-}else {
-  lastRes = currentSection;
-}
+// const father = document.querySelector(".swiper-wrapper-portfolio");
+// var container = document.querySelector(".port-redu");
+// var btns = container.querySelectorAll(".redu");
+// const subSection = document.querySelector(".redu.active");
+// let lastRes;
+// let currentSection  = subSection.innerText.toLowerCase().replaceAll(' ','-');//to fit with sections values from back-end
+// if(currentSection === "photos-taken-by-me") { // also to fit 
+//   lastRes = "photos";
+// }else {
+//   lastRes = currentSection;
+// }
 
-fetch("http://127.0.0.1:8000/api/portfolios").then((result)=>{
-  let myData = result.json();
-  return myData;
-}).then((myData) => {
-  const res = myData.portfolios.filter(it => it.section === lastRes
+// fetch("http://127.0.0.1:8000/api/portfolios").then((result)=>{
+//   let myData = result.json();
+//   return myData;
+// }).then((myData) => {
+//   const res = myData.portfolios.filter(it => it.section === lastRes
     
     
-    );
-  return res;
-}).then((res) => {
-  if(res.length === 0) {
-    console.log("no portfolios");
-  }else{
-    res.forEach(element => {
-      const s1 = document.createElement("div");
-      s1.classList.add("swiper-slide");
-      const s2 = document.createElement("div");
-      s2.classList.add("port-c-cont");
-      s1.append(s2);
-      const im = document.createElement("img");
-      im.setAttribute("src",`${element.image_url}`);
-      s2.append(im);
-      const s3 = document.createElement("div");
-      s3.classList.add("port-card-text");
-      s2.append(s3);
-      const p1 = document.createElement("p");
-      p1.innerText = `${element.name}`;
-      s3.append(p1);
-      const a1 = document.createElement("a");
-      a1.setAttribute("href",`${element.link}`);
-      a1.innerText = "view >";
-      s3.append(a1);
-      father.append(s1)
-    });
-  }
-})
+//     );
+//   return res;
+// }).then((res) => {
+//   if(res.length === 0) {
+//     console.log("no portfolios");
+//   }else{
+//     res.forEach(element => {
+//       const s1 = document.createElement("div");
+//       s1.classList.add("swiper-slide");
+//       const s2 = document.createElement("div");
+//       s2.classList.add("port-c-cont");
+//       s1.append(s2);
+//       const im = document.createElement("img");
+//       im.setAttribute("src",`${element.image_url}`);
+//       s2.append(im);
+//       const s3 = document.createElement("div");
+//       s3.classList.add("port-card-text");
+//       s2.append(s3);
+//       const p1 = document.createElement("p");
+//       p1.innerText = `${element.name}`;
+//       s3.append(p1);
+//       const a1 = document.createElement("a");
+//       a1.setAttribute("href",`${element.link}`);
+//       a1.innerText = "view >";
+//       s3.append(a1);
+//       father.append(s1)
+//     });
+//   }
+// })
 
 
 
 // acive and disactive in port-redu links
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-    while (father.hasChildNodes()) {
-      father.removeChild(father.firstChild);
-    }
-    currentSection  = this.innerText.toLowerCase().replaceAll(' ','-');
-  if(currentSection === "photos-taken-by-me") { // also to fit 
-  lastRes = "photos";
-}else {
-  lastRes = currentSection;
-}
-  fetch("http://127.0.0.1:8000/api/portfolios").then((result)=>{
-  let myData = result.json();
-  return myData;
-}).then((myData) => {
-  const res = myData.portfolios.filter(it => it.section === lastRes
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//     while (father.hasChildNodes()) {
+//       father.removeChild(father.firstChild);
+//     }
+//     currentSection  = this.innerText.toLowerCase().replaceAll(' ','-');
+//   if(currentSection === "photos-taken-by-me") { // also to fit 
+//   lastRes = "photos";
+// }else {
+//   lastRes = currentSection;
+// }
+//   fetch("http://127.0.0.1:8000/api/portfolios").then((result)=>{
+//   let myData = result.json();
+//   return myData;
+// }).then((myData) => {
+//   const res = myData.portfolios.filter(it => it.section === lastRes
     
     
-    );
-  return res;
-}).then((res) => {
-  if(res.length === 0) {
-    console.log("no portfolios");
-  }else{
-    res.forEach(element => {
-      const s1 = document.createElement("div");
-      s1.classList.add("swiper-slide");
-      const s2 = document.createElement("div");
-      s2.classList.add("port-c-cont");
-      s1.append(s2);
-      const im = document.createElement("img");
-      im.setAttribute("src",`${element.image_url}`);
-      s2.append(im);
-      const s3 = document.createElement("div");
-      s3.classList.add("port-card-text");
-      s2.append(s3);
-      const p1 = document.createElement("p");
-      p1.innerText = `${element.name}`;
-      s3.append(p1);
-      const a1 = document.createElement("a");
-      a1.setAttribute("href",`${element.link}`);
-      a1.innerText = "view >";
-      s3.append(a1);
-      father.append(s1)
-    });
-  }
-})
-  });
-}
+//     );
+//   return res;
+// }).then((res) => {
+//   if(res.length === 0) {
+//     console.log("no portfolios");
+//   }else{
+//     res.forEach(element => {
+//       const s1 = document.createElement("div");
+//       s1.classList.add("swiper-slide");
+//       const s2 = document.createElement("div");
+//       s2.classList.add("port-c-cont");
+//       s1.append(s2);
+//       const im = document.createElement("img");
+//       im.setAttribute("src",`${element.image_url}`);
+//       s2.append(im);
+//       const s3 = document.createElement("div");
+//       s3.classList.add("port-card-text");
+//       s2.append(s3);
+//       const p1 = document.createElement("p");
+//       p1.innerText = `${element.name}`;
+//       s3.append(p1);
+//       const a1 = document.createElement("a");
+//       a1.setAttribute("href",`${element.link}`);
+//       a1.innerText = "view >";
+//       s3.append(a1);
+//       father.append(s1)
+//     });
+//   }
+// })
+//   });
+// }
+// <section id="portfolio-section" class="blog-sec">
+// <h2 class="main-heading">Blog</h2>
+// <div class="portfolio-contanier port-content">
+//   <ul class="port-redu">
+// 	<li class="redu active">Lorem</li>
+// 	<li class="redu">Lorem</li>
+// 	<li class="redu">Lorem</li>
+// 	<li class="redu">Lorem</li>
+//   </ul>
+//   <div class="swiper blog-swiper">
+// 	<div class="swiper-wrapper swiper-wrapper-portfolio">
+// 	  <div class="swiper-slide">
+// 		<div class="port-c-cont">
+// 		  <img src="./imgs/blog-photo.png" alt="" />
+// 		  <div class="blog-card-text">
+// 			<h2>Lorem ipsum dolor sit amet</h2>
+// 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda vitae cumque accusantium.</p>
+// 			<div class="blog-carf-lastone">
+// 			  <a href="">Read more</a>
+// 			  <p>22th Aug 2023</p>
+// 			</div>
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	  <div class="swiper-slide">
+// 		<div class="port-c-cont">
+// 		  <img src="./imgs/blog-photo.png" alt="" />
+// 		  <div class="blog-card-text">
+// 			<h2>Lorem ipsum dolor sit amet</h2>
+// 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda vitae cumque accusantium.</p>
+// 			<div class="blog-carf-lastone">
+// 			  <a href="">Read more</a>
+// 			  <p>22th Aug 2023</p>
+// 			</div>
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	  <div class="swiper-slide">
+// 		<div class="port-c-cont">
+// 		  <img src="./imgs/blog-photo.png" alt="" />
+// 		  <div class="blog-card-text">
+// 			<h2>Lorem ipsum dolor sit amet</h2>
+// 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda vitae cumque accusantium.</p>
+// 			<div class="blog-carf-lastone">
+// 			  <a href="">Read more</a>
+// 			  <p>22th Aug 2023</p>
+// 			</div>
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	  <div class="swiper-slide">
+// 		<div class="port-c-cont">
+// 		  <img src="./imgs/blog-photo.png" alt="" />
+// 		  <div class="blog-card-text">
+// 			<h2>Lorem ipsum dolor sit amet</h2>
+// 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda vitae cumque accusantium.</p>
+// 			<div class="blog-carf-lastone">
+// 			  <a href="">Read more</a>
+// 			  <p>22th Aug 2023</p>
+// 			</div>
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	  <div class="swiper-slide">
+// 		<div class="port-c-cont">
+// 		  <img src="./imgs/blog-photo.png" alt="" />
+// 		  <div class="blog-card-text">
+// 			<h2>Lorem ipsum dolor sit amet</h2>
+// 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda vitae cumque accusantium.</p>
+// 			<div class="blog-carf-lastone">
+// 			  <a href="">Read more</a>
+// 			  <p>22th Aug 2023</p>
+// 			</div>
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	  <div class="swiper-slide">
+// 		<div class="port-c-cont">
+// 		  <img src="./imgs/blog-photo.png" alt="" />
+// 		  <div class="blog-card-text">
+// 			<h2>Lorem ipsum dolor sit amet</h2>
+// 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error assumenda vitae cumque accusantium.</p>
+// 			<div class="blog-carf-lastone">
+// 			  <a href="">Read more</a>
+// 			  <p>22th Aug 2023</p>
+// 			</div>
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	  </div>
+// 	</div>
+// 	<div class="bottom-row">
+// 	  <div class="swiper-button-next"></div>
+// 	  <div class="swiper-button-prev"></div>
+// 	  <div class="swiper-pagination"></div>
+// 	</div>
+//   </div>
+// </div>
+// </section>
